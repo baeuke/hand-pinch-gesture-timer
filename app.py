70b5c5716ff426a4772bd295389ff0c4f9419e36
Baurzhan Abenov <baurzhan.abenov@nu.edu.kz>
2024-03-22 16:05:47 -0700

    debugged, removed modes

--- app.py
+++ app.py
@@ -97,7 +97,7 @@ def main():
         debug_image = copy.deepcopy(image)
 
         # Detection implementation #############################################################
-        image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
+        # image = cv.cvtColor(image, cv.COLOR_BGR2RGB)
 
         image.flags.writeable = False
         results = hands.process(image)
